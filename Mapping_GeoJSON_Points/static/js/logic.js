b765f3ad180cbe35d209070bfe8fac3bba6a6873
tanahildebrand <tana.hildebrand@gmail.com>
2023-02-11 11:50:06 -0500

    Updated Logic file for single point

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,28 +1,45 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([39.833333, -98.583333], 5);
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"14",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [37.6214, -122.3790], //SFO
-  [30.1975, -97.6664], //AUS
-  [39.9999, -82.8872], //CMH
-  [43.6777, -79.6248], //YYZ
-  [40.6413, -73.7781] //JFK
-];
+// Grabbing our GeoJSON data and use "Point to Layer" function for the popup
+//L.geoJSON(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+  //pointToLayer: function(feature, latlng) {
+    //console.log(feature);
+    //return L.marker(latlng)
+    //.bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city + ", " +feature.properties.country + "</h3>");
+  //}
 
-// Create a polyline using the line coordinates and make the line yellow.
-L.polyline(line, {
-  color: "blue",
-  opacity: 0.5,
-  weight: 4,
-  dashArray: "5,10"
+  // Grabbing our GeoJSON data and use "onEachFeature " function for the popup
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup("<h2> Airport Code:" + feature.properties.faa + "</h2> <hr> <h3> Airport Name:" + feature.properties.name + "</h3>");
+  }
 }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
